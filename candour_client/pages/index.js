5dff410e45bb9f213e345e629e3414ff50d030df
oversparkling <56868622+oversparkling@users.noreply.github.com>
2022-02-09 21:46:48 +0800

    Tailwind config and revamp index page

--- candour_client/pages/index.js
+++ candour_client/pages/index.js
@@ -4,6 +4,7 @@ import detectEthereumProvider from "@metamask/detect-provider";
 
 import Web3 from "web3";
 import getWeb3 from "./getWeb3";
+import { Button, Input } from "antd";
 
 const App = () => {
     const [state, setState] = useState({
@@ -11,7 +12,13 @@ const App = () => {
         accounts: null,
         contract: null,
     });
-    const [batchNo, setBatchNo] = useState();
+    const [registrationNo, setRegistrationNo] = useState("");
+    const [farmerName, setFarmerName] = useState("");
+    const [farmAddress, setFarmAddress] = useState("");
+    const [exporterName, setExporterName] = useState("");
+    const [importerName, setImporterName] = useState("");
+    const [batchNo, setBatchNo] = useState("");
+    const [returnValue, setReturnValue] = useState(null);
     useEffect(() => {
         const init = async () => {
             try {
@@ -47,7 +54,13 @@ const App = () => {
         );
         console.log(state.contract.options);
         const batchNo1 = await state.contract.methods
-            .setBasicDetails("test", "test", "test", "test", "test")
+            .setBasicDetails(
+                registrationNo,
+                farmerName,
+                farmAddress,
+                exporterName,
+                importerName
+            )
             .send({ from: accounts[0], gasPrice: "200" });
         console.log(batchNo1.events.CottonHarvested.returnValues.batchNo);
         setBatchNo(batchNo1.events.CottonHarvested.returnValues.batchNo);
@@ -68,29 +81,100 @@ const App = () => {
         const returnValue = await state.contract.methods
             .getBasicDetails(batchNo)
             .send({ from: accounts[0], gasPrice: "200" });
-        console.log(returnValue)
+        console.log(returnValue);
+        setReturnValue(returnValue);
         // console.log(test.call().send({ from: state.accounts[0] }))
     };
     if (!state.web3) {
         return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-        <div className="App">
-            <h1>Good to Go!</h1>
-            <p>Your Truffle Box is installed and ready.</p>
-            <h2>Smart Contract Example</h2>
-            <p>
-                If your contracts compiled and migrated successfully, below will
-                show a stored value of 5 (by default).
-            </p>
-            <p>
-                Try changing the value stored on <strong>line 42</strong> of
-                App.js.
-            </p>
-            <button onClick={() => setBasicDetails()}>setBasicDetails</button>
-            <button onClick={() => getBasicDetails(batchNo)}>
+        <div className="flex-col flex items-center justify-center">
+            <h1 className="text-3xl font-bold">Welcome to Candour </h1>
+            {batchNo != "" ? (
+                <span>
+                    Your batch number is:{" "}
+                    <span className="font-bold">{batchNo}</span>{" "}
+                </span>
+            ) : (
+                <span></span>
+            )}
+            <p>Your Truffle Box is installed and ready</p>
+            <input
+                className="border mt-5 px-5"
+                placeholder="Registration No."
+                onChange={(e) => setRegistrationNo(e.target.value)}
+            ></input>
+            <input
+                className="border mt-5 px-5"
+                placeholder="Farmer Name"
+                onChange={(e) => setFarmerName(e.target.value)}
+            ></input>
+            <input
+                className="border mt-5 px-5"
+                placeholder="Farm Address"
+                onChange={(e) => setFarmAddress(e.target.value)}
+            ></input>
+            <input
+                className="border mt-5 px-5"
+                placeholder="Exporter Name"
+                onChange={(e) => setExporterName(e.target.value)}
+            ></input>
+            <input
+                className="border mt-5 px-5"
+                placeholder="Importer Name"
+                onChange={(e) => setImporterName(e.target.value)}
+            ></input>
+            <Button className="mt-5" onClick={() => setBasicDetails()}>
+                setBasicDetails
+            </Button>
+            <div className="flex-row flex w-1/3 justify-center ">
+                {returnValue != null ? (
+                    <div className="p-8 flex-col flex items-center">
+                        <span>
+                            Registration No. is:{" "}
+                            {
+                                returnValue.events.CottonBatchNo.returnValues
+                                    .registrationNo
+                            }
+                        </span>
+                        <span>
+                            Farmer's name is:{" "}
+                            {
+                                returnValue.events.CottonBatchNo.returnValues
+                                    .farmerName
+                            }
+                        </span>
+
+                        <span>
+                            Farm Address is:{" "}
+                            {
+                                returnValue.events.CottonBatchNo.returnValues
+                                    .farmAddress
+                            }
+                        </span>
+                        <span>
+                            Exporter Name is:{" "}
+                            {
+                                returnValue.events.CottonBatchNo.returnValues
+                                    .exporterName
+                            }
+                        </span>
+                        <span>
+                            Importer Name is:{" "}
+                            {
+                                returnValue.events.CottonBatchNo.returnValues
+                                    .importerName
+                            }
+                        </span>
+                    </div>
+                ) : (
+                    <div className="p-8"></div>
+                )}
+            </div>
+            <Button onClick={() => getBasicDetails(batchNo)}>
                 getBasicDetails
-            </button>
+            </Button>
         </div>
     );
 };
