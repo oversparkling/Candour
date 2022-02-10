5139cd97b5fa1981aa7488e91f0faf006821d65a
Oversparkling <nicholasongck@gmail.com>
2022-02-10 18:48:15 +0800

    im sorry for the bad messges

--- contracts/ClothingSupplyChain.sol
+++ contracts/ClothingSupplyChain.sol
@@ -14,6 +14,7 @@ contract ClothingSupplyChain {
 
     struct basicDetails{
         string registrationNo;
+        
         string farmerName;
         string farmAddress;
         string exporterName;
