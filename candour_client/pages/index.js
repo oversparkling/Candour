2da4e2e86498c4ae44af2617a054b353cbd11b00
Alvin Owyong <alvinowyong@gmail.com>
2022-03-20 04:31:19 +0800

    pdf and image scanner

--- candour_client/pages/index.js
+++ candour_client/pages/index.js
@@ -1,14 +1,38 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { Component, useEffect, useState, useRef } from "react";
 import ClothingSupplyChain from "../../build/contracts/ClothingSupplyChain.json";
 import detectEthereumProvider from "@metamask/detect-provider";
-import Image from "next/image";
 import {useRouter} from "next/router";
 import Web3 from "web3";
 import getWeb3 from "./getWeb3";
-import { Button, Input } from "antd";
 import Loading from "./Loading";
+import QrcodeDecoder from 'qrcode-decoder';
+import { notification } from 'antd';
 
 const App = () => {
+    const openNotification = placement => {
+        notification.info({
+            message: `File Uploaded`,
+            description: 'Please hold while we process your document.',
+            placement,
+        });
+    };
+
+    const successNotification = placement => {
+        notification.success({
+            message: `Success`,
+            description: 'Batch number was found in QR Code. You will be redirected shortly. ',
+            placement,
+        });
+    };
+
+    const failureNotification = placement => {
+        notification.error({
+            message: `An error has occured.`,
+            description: 'Unable to detect QR Code in file. Please try again with another file.',
+            placement,
+        });
+    };
+
     const [state, setState] = useState({
         web3: null,
         accounts: null,
@@ -16,6 +40,11 @@ const App = () => {
     });
     const [loading,setLoading] = useState(true)
     const router = useRouter()
+    const uploadQR = useRef(null);
+    var qr = new QrcodeDecoder();
+    const iconHandleClick = () => {
+        uploadQR.current.click()
+    }
     const [registrationNo, setRegistrationNo] = useState("");
     const [farmerName, setFarmerName] = useState("");
     const [farmAddress, setFarmAddress] = useState("");
@@ -24,6 +53,7 @@ const App = () => {
     const [batchNo, setBatchNo] = useState("");
     const [returnValue, setReturnValue] = useState(null);
     const [allDetails, setAllDetails] = useState();
+    
     useEffect(() => {
         const init = async () => {
             try {
@@ -48,6 +78,61 @@ const App = () => {
         // "0x69b6bF0d2Daaad6cBEbBd62e4AEA94ce5Cf93eF4"
         console.log(state.web3);
     }, []);
+
+    const handleUpload = async (event) => {
+        openNotification('topRight')
+        try {
+            if (event.target.files[0].name.split(".").pop() == 'pdf') {
+                var file = event.target.files[0];
+                var fileReader = new FileReader();
+                fileReader.onload = async function () {
+                    const canvas = document.createElement("canvas");
+                    const pdfJS = require("pdfjs-dist/build/pdf.js");
+                    const pdfJSWorker = require("pdfjs-dist/build/pdf.worker.entry.js");
+                    pdfJS.GlobalWorkerOptions.workerSrc = pdfJSWorker;
+
+                    var typedarray = new Uint8Array(this.result);
+                    const pdf = await pdfJS.getDocument(typedarray).promise;
+
+                    for (let i = 0; i < pdf.numPages; i++) {
+                        const page = await pdf.getPage(i + 1);
+                        const viewport = page.getViewport({ scale: 2 });
+                        const context = canvas.getContext("2d");
+                        canvas.height = viewport.height * 2;
+                        canvas.width = 2480 * 2;
+                        await page.render({ canvasContext: context, viewport: viewport }).promise;
+                        canvas.toBlob(function (blob) {
+                            var image = URL.createObjectURL(blob);
+                            qr.decodeFromImage(image).then((res) => {
+                                setBatchNo(res.data.split("batchno?batchno=")[1]);
+                                successNotification('topRight');
+                            }).catch(function (error) {
+                                console.log("Unable to detect QR Code");
+                            });
+                        });
+                    }
+                }
+                fileReader.readAsArrayBuffer(file)
+            } else {
+                let image = URL.createObjectURL(event.target.files[0]);
+                qr.decodeFromImage(image).then((res) => {
+                    console.log(res.data)
+                    setBatchNo(res.data.split("batchno?batchno=")[1]);
+                    if (res.data.split("batchno?batchno=")[1] !== undefined) {
+                        successNotification('topRight');
+                    } else {
+                        throw "exit"
+                    }
+                }).catch(function (error) {
+                    failureNotification('topRight');
+                    console.log("Unable to detect QR Code");
+                });
+            }
+        } catch (error) {
+            console.log("An error occured")
+        }
+    }
+
     const setBasicDetails = async () => {
         const provider = await detectEthereumProvider();
         const web3 = new Web3(provider);
@@ -279,7 +364,7 @@ const App = () => {
         //         <span onClick={()=>router.push('/newbatch')} className="text-xs text-blue-500 cursor-pointer underline mt-2">Not this? Start a new batch</span>
         //     </div>
         // </div>
-        <div className="w-full h-screen flex-col flex pt-7 pl-4 pr-4 lg:pb-10 pb-28 bg-center bg-cover" style={{ backgroundImage: 'url(' + "https://i.im.ge/2022/03/18/lxoEWF.png" + ')' }}>
+        <div className="w-full h-screen flex-col flex pt-7 px-4 lg:pb-10 pb-28 bg-standard bg-cover lg:px-10" style={{ backgroundPosition: "left top" }}>
             <div className="grid grid-cols-6 gap-4 pb-6">
                 <div className="col-start-1 col-span-2 pl-5"><img
                     src="/navbarlogo.png"
@@ -291,39 +376,42 @@ const App = () => {
                 </div>
             </div>
 
-            <div className="w-full h-full bg-white shadow-lg rounded-md px-5 pt-5 text-center lg:h-full">
-                <h3 className="font-semibold text-2xl text-gray-600 font-sans pb-0 mb-0 pt-3">Scan or enter batch number.</h3>
-                <p className="text-gray-500 text-xs">Batch number are usually found on products and receipts.<br />Otherwise, liaise with your suppliers to find out about .</p>
-                <img src="/scan.gif" className="p-8 inline h-max w-max lg:h-2/5 md:h-3/5 sm:h-3/5" />
-                <div className=" w-full mt-2">
-                    <input id="default" className="w-3/5 border p-3 rounded-lg text-xs" placeholder="Product Batch No." onChange={(e) => setBatchNo(e.target.value)} />
+            <div className="w-full h-full bg-white shadow-lg rounded-lg px-5 pt-5 lg:h-full lg:px-7 lg:text-center md:text-center">
+                <h3 className="font-header text-xl tracking-tighter mb-1 text-black-900 lg:text-2xl lg:pt-2">Add / View Batch</h3>
+                <p className="text-black-800 text-xs tracking-tighter leading-none font-prints lg:text-sm">Upload / scan receipt or input product batch number to begin. Contact your suppliers to find out more.</p>
+                <div className="lg:h-1/3 md:h-1/3 sm:h-1/3 h-1/3 text-center">
+                    <img src="/qrscan.gif" className="p-8 inline h-full w-auto" />
                 </div>
-                {/* <button className="md:w-1/2 p-3 w-full bg-black rounded-lg font-bold py-2 px-4 rounded inline-flex items-center" onClick={()=>console.log(state.accounts[0])}>
-                    <span className="text-white font-semibold text-xs">Scan QR Code</span>
-                </button> */}
-                <button type="submit" className="mt-3 inline-flex w-3/5 justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mr-2 bi bi-qr-code-scan" viewBox="0 0 16 16">
-                        <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0v-3Zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5ZM.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5Zm15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5ZM4 4h1v1H4V4Z" />
-                        <path d="M7 2H2v5h5V2ZM3 3h3v3H3V3Zm2 8H4v1h1v-1Z" />
-                        <path d="M7 9H2v5h5V9Zm-4 1h3v3H3v-3Zm8-6h1v1h-1V4Z" />
-                        <path d="M9 2h5v5H9V2Zm1 1v3h3V3h-3ZM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8H8Zm2 2H9V9h1v1Zm4 2h-1v1h-2v1h3v-2Zm-4 2v-1H8v1h2Z" />
-                        <path d="M12 9h2V8h-2v1Z" />
-                    </svg>
-                    Scan QR Code
-                </button>
-                <br />
-                <button onClick={() => router.push({
-                    pathname: "/batchno", query: {
-                        batchno: batchNo
-                    }
-                })}
-                    type="submit" class="mt-1 inline-flex w-3/5 justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mr-2 bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
-                        <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z" />
-                    </svg>
-                    Continue</button>
-                <br />
-                <span onClick={() => router.push('/newbatch')} className="text-xs text-blue-500 cursor-pointer underline mt-2">Not this? Start a new batch</span>
+                <div className=" w-full mt-2 text-center lg:px-20">
+                    <label htmlFor="website-admin" className="block mb-1 font-semibold text-black-900 tracking-tighter text-left">Batch Number</label>
+                    <div className="flex justify-center">
+                        <div className="w-full inline-flex h-12">
+                            <input onChange={(e) => setBatchNo(e.target.value)} type="text" value={batchNo} className="focus:border-blue-900 rounded-none rounded-l-lg border outline-0 border-gray-300 text-gray-900 text-xs font-prints block flex-1 min-w-0 w-full text-xs border-gray-300 p-2.5" placeholder="e.g. 0x2108429832e29832" />
+                            <input ref={uploadQR} id="uploadQR" type="file" style={{ display: "none" }} accept="application/pdf,image/*" capture="environment" onChange={(e) => handleUpload(e)} />
+                            <span onClick={iconHandleClick} className="border-blue-900 inline-flex items-center px-3 text-sm text-gray-900 rounded-r-md border border-l-0 bg-blue-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" className="m-2 bi bi-qr-code-scan" viewBox="0 0 16 16">
+                                    <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0v-3Zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5ZM.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5Zm15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5ZM4 4h1v1H4V4Z" />
+                                    <path d="M7 2H2v5h5V2ZM3 3h3v3H3V3Zm2 8H4v1h1v-1Z" />
+                                    <path d="M7 9H2v5h5V9Zm-4 1h3v3H3v-3Zm8-6h1v1h-1V4Z" />
+                                    <path d="M9 2h5v5H9V2Zm1 1v3h3V3h-3ZM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8H8Zm2 2H9V9h1v1Zm4 2h-1v1h-2v1h3v-2Zm-4 2v-1H8v1h2Z" />
+                                    <path d="M12 9h2V8h-2v1Z" />
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+                    <button onClick={() => router.push({
+                        pathname: "/batchno", query: {
+                            batchno: batchNo
+                        }
+                    })}
+                        type="submit" className="h-12 bg-blue-900 font-header tracking-tighter mt-3 inline-flex mb-5 w-full items-center justify-center text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="self-center mr-2 bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+                            <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z" />
+                        </svg>
+                        Continue</button>
+                    <br /><span className="text-xs text-black-900 cursor-pointer mt-2 font-header">Not this? </span>
+                    <span onClick={() => router.push('/newbatch')} className="text-sm tracking-tighter text-blue-900 cursor-pointer mt-2 font-header hover:underline">Create new batch</span>
+                </div>
             </div>
         </div>
     );
