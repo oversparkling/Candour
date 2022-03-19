6c3f3342ae58c30bf2b089442f5afa55696732dc
Alvin Owyong <alvinowyong@gmail.com>
2022-03-20 04:32:21 +0800

    config file changes

--- candour_client/tailwind.config.js
+++ candour_client/tailwind.config.js
@@ -4,7 +4,34 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'standard': "url('/bg.png')",
+                'teal': "url('/bg_teal.png')",
+                'green': "url('/bg_green.png')",
+                'red': "url('/bg_red.png')",
+            },
+            colors: {
+                black: {
+                    900: '#3D4257',
+                    800: '#747782',
+                },
+                blue: {
+                    900: '#007AFF',
+                },
+                green: {
+                    50: '#E1F7EC',
+                    800: '#236C4D',
+                    900: '#4BAE4F',
+                },
+            },
+            fontFamily: {
+                header: ["SF Pro Semibold"],
+                prints: ["SF Pro Regular"],
+                title: ["SF Pro Fullbold"],
+                comic: ["comic"]
+            },
+        },
     },
     plugins: [],
 };
